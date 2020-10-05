c93c7a37d47b46e6289fa5bc4283d0066a463c69
MikeIsNewToTheCity <mikevilleneuve@gmail.com>
2020-10-05 13:50:04 -0500

    Added proper async/await with try/catch

--- src/App.js
+++ src/App.js
@@ -2,15 +2,32 @@ import React, { useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { API } from 'aws-amplify';
+import { response } from '../amplify/backend/function/cryptofunction/src/app';
 
 const App = () => {
 
   const [coins, updateCoins] = useState([]);
 
   const fetchCoins = async () => {
-    const data = await API.get('cryptoapi', `/coins?limit=${input.limit}&start=${input.start}`);
-    updateCoins(data.coins);
+    try {
+      const data = await API.get('cryptoapi', `/coins?limit=${input.limit}&start=${input.start}`);
+      updateCoins(data.coins);
+    }
+    catch(err) {
+      console.error(err);
+    }
   }  
+
+  // const fetchCoinsn = () => {
+  //   API.get('cryptoapi', `/coins?limit=${input.limit}&start=${input.start}`)
+  //   .then(response => {
+  //     console.log(response)
+  //     updateCoins(response.coins);
+  //   })
+  //   .catch(err => {
+  //     console.log(err)
+  //   });
+  // }
 
   useEffect(
     () => {
