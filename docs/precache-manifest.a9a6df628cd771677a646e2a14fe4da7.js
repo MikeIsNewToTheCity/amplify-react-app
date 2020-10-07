27b18638e53135b0694919dfe6870e98001c472e
MikeIsNewToTheCity <mikevilleneuve@gmail.com>
2020-10-07 13:04:41 -0500

    Attempting Github pages publish

---
+++ docs/precache-manifest.a9a6df628cd771677a646e2a14fe4da7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abdb8f966528e7f64f11ce0b818f4425",
+    "url": "/amplify-react-app/index.html"
+  },
+  {
+    "revision": "4f35977f957810e0418d",
+    "url": "/amplify-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "640594636de2ce982708",
+    "url": "/amplify-react-app/static/js/2.058d3324.chunk.js"
+  },
+  {
+    "revision": "3d4d9872e604682782346535cc277e64",
+    "url": "/amplify-react-app/static/js/2.058d3324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f35977f957810e0418d",
+    "url": "/amplify-react-app/static/js/main.6e0ec637.chunk.js"
+  },
+  {
+    "revision": "47435f97aff3063d93de",
+    "url": "/amplify-react-app/static/js/runtime-main.697215da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/amplify-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
