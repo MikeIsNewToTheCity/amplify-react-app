94889f155c1b2c900bce55093d7c1c8dd1c1e45a
MikeIsNewToTheCity <mikevilleneuve@gmail.com>
2020-12-16 20:22:18 -0600

    Setup GitHubBornOn component

--- src/GitHubBornOn.js
+++ src/GitHubBornOn.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
+import { API } from 'aws-amplify';
+import './App.css';
 
 const GitHubBornOn = () => {
 
+    const [bornOn, setBornOn] = useState({ username: 'unknown', date: 'unknown' });
+
+    const fetchBornOn = async () => {
+      const data = await API.get('cryptoapi', '/born');
+      setBornOn({
+        username: data.borninfo.login
+        , date: data.borninfo.created_at
+      });
+    }
+  
+    useEffect(
+      () => {
+        fetchBornOn();
+      }
+      , []
+    );
+
     return (
         <h3>
-            mikeisnewtothecity - Placeholder
+            { bornOn.username } - { bornOn.date }
         </h3>
-    )
-
+    );
 }
 
 export default GitHubBornOn;
